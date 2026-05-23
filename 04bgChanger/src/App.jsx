2aa4ceee2c8888c99c4e6cc2453a07b6b9851926
Divyanshu0902 <divyanshu.kumar.it28@heritageit.edu>
2026-05-23 09:07:43 +0530

    HTML and CSS created for bgChanger project. Now I'll add React logic.

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="viewport">
+        <div className="navbar">
+            <div className="button Violet">Violet</div>
+            <div className="button Indigo">Indigo</div>
+            <div className="button Blue">Blue</div>
+            <div className="button Green">Green</div>
+            <div className="button Yellow">Yellow</div>
+            <div className="button Orange">Orange</div>
+            <div className="button Red">Red</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
